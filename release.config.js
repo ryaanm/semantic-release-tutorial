5fbb24814acbb8ed2f53f01faefad84b3af4792a
rayen_merghad <rayen@keltio.fr>
2023-02-23 12:22:58 +0100

    feat(new): angular

--- release.config.js
+++ release.config.js
@@ -1,17 +1,4 @@
 module.exports = {
     branches: ['main'],
-    plugins: [
-      '@semantic-release/commit-analyzer',
-      '@semantic-release/release-notes-generator',
-      [
-        "@semantic-release/changelog",
-        {
-          "changelogFile": "./CHANGELOG.md"
-        }
-      ],
-      '@semantic-release/npm',
-      '@semantic-release/git',
-      '@semantic-release/github',
-    ],
     "dryRun": false
   };
